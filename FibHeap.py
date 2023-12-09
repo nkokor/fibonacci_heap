3952f4028dcc5fe6f7bceec78e29973d2354d9f2
Nikolina Kokor <nkokor@icloud.com>
2023-12-09 22:37:04 +0100

    fib_heap_insert method implemented

--- FibHeap.py
+++ FibHeap.py
@@ -3,8 +3,28 @@ class FibHeap():
     self.n = 0
     self.min = None
 
-  def make_fib_heap():
-    fib_heap = FibHeap()
-    return fib_heap
-  
-  
+  # asuming that x is already an initialized node with set key 
+  def fib_heap_insert(self, x):
+    x.degree = 0
+    x.p = None
+    x.child = None
+    x.mark = False
+
+    # if heap is empty x is the only root and heap minimum
+    if self.min is None:
+      self.root_list = [x]
+      self.min = x
+
+    # if heap is not empty x is inserted into root list
+    else:
+      min_left = self.min.left
+      self.min.left = x
+      x.right = self.min
+      if min_left is not None:
+        min_left.right = x
+        x.left = min_left
+      self.root_list.append(x)
+
+      if x.key < self.min.key:
+        self.min = x
+    self.n = self.n + 1
--- FibNode.py
+++ FibNode.py
@@ -1,10 +1,19 @@
 class Node():
   def __init__(self, key):
     self.p = None
+
+    # one of node's children 
     self.child = None
+
+    # left sibling of node
     self.left = None
+    # right sibling of node
     self.right = None
+
+    # number of node's children
     self.degree = 0
+
+    # node is marked when it loses a child after it became a child of another node
     self.marked = False
     self.key = key
     
